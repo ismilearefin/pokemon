ed11f04ce7f095d611e7348a84355e600ef45828
ismilearefin <arefinismile@gmail.com>
2023-01-16 03:14:27 +0600

    about sec

--- src/components/Home/Home/AboutSec/AboutSec.js
+++ src/components/Home/Home/AboutSec/AboutSec.js
@@ -18,14 +18,41 @@ const AboutSec = () => {
                 <p className='font-[Ubuntu-Bold] leading-[48px] text-center font-bold text-5xl text-[#257BC4]'>Ash & Pikachu Arrive in <br/> Pokémon Universe</p>
             </div>
 
-            <div className='w-[1546px]'>
-                <div className=''>
-                    <div className='grid grid-cols-3 '>
-                    <img className='row-span-3 mt-10px' src={image1} alt='image01'/>
-                    <img className='col-span-1' src={image2} alt='image02'/>
-                    <img className='row-span-2' src={image3} alt='image03'/>
-                    <img className='' src={image4} alt='image04'/>
-                    <img className='' src={image5} alt='image05'/>
+            <div className='w-[1546px] mb-24'>
+                <div className='font-[Ubuntu-Regular] text-base font-normal text-justify leading-[18px]'>
+                    <div className='grid grid-cols-7 grid-row-6  gap-4'>
+                   {/* 01 */}
+                    <p className='col-span-3 '>Lorem ipsum dolor sit amet consectetur. Risus cursus nibh elementum ornare a aliquet ac. Feugiat scelerisque ultrices tempor facilisi tempus risus nunc. Proin quis morbi posuere nisl etiam scelerisque. Proin pretium gravida semper ut erat nisi. Pulvinar ac mattis porta amet et. Nisl urna non fames felis leo. Vitae pulvinar sed viverra sit pretium lorem elementum. Iaculis sit maecenas sodales mi convallis justo aliquam. Tincidunt semper ut ornare vivamus lectus.</p>
+                    {/* 2 */}
+                    <img className='row-span-2 w-[200px] items-center' src={image1} alt='image01'/>
+                    {/* 3 */}
+                    <p className='col-span-3 '>Lorem ipsum dolor sit amet consectetur. Risus cursus nibh elementum ornare a aliquet ac. Feugiat scelerisque ultrices tempor facilisi tempus risus nunc. Proin quis morbi posuere nisl etiam scelerisque. Proin pretium gravida semper ut erat nisi. Pulvinar ac mattis porta amet et. Nisl urna non fames felis leo. Vitae pulvinar sed viverra .</p>
+                    {/* 4 */}
+                    <p className='col-span-2 row-span-2'>Lorem ipsum dolor sit amet consectetur. Turpis integer massa consectetur sed enim quis viverra. Vestibulum eu nibh dolor semper. Nisl feugiat quis nec odio pulvinar feugiat velit. Nulla massa sit morbi morbi. Tortor viverra eget lacus feugiat. Tempus vitae vitae orci at ultrices nisi diam faucibus. Ultricies in cursus volutpat aliquam turpis urna in sed. Hendrerit arcu sit lectus adipiscing egestas semper nunc. Ante consectetur id congue pulvinar libero tristique et orci. Platea convallis dictum dui augue. Tincidunt mattis urna sit semper sed duis feugiat mi.</p>
+                    {/* 5 */}
+                    <img  className='row-span-2 col-span-1 w-[200px] items-center' src={image3} alt='image03'/>
+                    {/* 6 */}
+                    {/* 8 */}
+                    <img className='w-[200px] row-span-2 col-span-1 items-center' src={image4} alt='image04'/>
+                    {/* 7 */}
+                    <p className='col-span-2 row-span-2'>Lorem ipsum dolor sit amet consectetur. Turpis integer massa consectetur sed enim quis viverra. Vestibulum eu nibh dolor semper. Nisl feugiat quis nec odio pulvinar feugiat velit. Nulla massa sit morbi morbi. Tortor viverra eget lacus feugiat. Tempus vitae vitae orci at ultrices nisi diam faucibus. Ultricies in cursus volutpat aliquam turpis urna in sed. Hendrerit arcu sit lectus adipiscing egestas semper nunc. Ante consectetur id congue pulvinar libero tristique et orci. Platea convallis dictum dui augue. Tincidunt mattis urna sit semper sed duis feugiat mi.Ante consectetur id congue </p>
+                    {/* 9 */}
+                    <img className='row-span-2  w-[200px] items-center' src={image2} alt='image02'/>
+                    <p className='col-span-3 '>Lorem ipsum dolor sit amet consectetur. Tincidunt at cras tortor non volutpat quisque facilisis. Ultricies consequat sed vitae ac. Nisl eu nam id lectus tellus sit egestas. Orci iaculis et vehicula nisi facilisi neque lorem. In vulputate feugiat lobortis eros viverra. Turpis viverra vel fames enim tortor. Scelerisque dictumst aliquam gravida eget ut accumsan. A est dis platea vitae blandit quis. Ultricies ac at urna vel morbi diam.
+Donec ut sit sit et. Etiam cum faucibus eu elementum ut fermentum in cursus.</p>
+                    {/* 10 */}
+                    <img className='w-[200px] row-span-2 items-center' src={image5} alt='image05'/>
+                    {/* 11 */}
+                    <p className='col-span-2 row-span-2'>Lorem ipsum dolor sit amet consectetur. Tincidunt at cras tortor non volutpat quisque facilisis. Ultricies consequat sed vitae ac. Nisl eu nam id lectus tellus sit egestas. Orci iaculis et vehicula nisi facilisi neque lorem. In vulputate feugiat lobortis eros viverra. Turpis viverra vel fames enim tortor. Scelerisque dictumst aliquam gravida eget ut accumsan. A est dis platea vitae blandit quis. Ultricies ac at urna vel morbi diam.
+Donec ut sit sit et. Etiam cum faucibus eu elementum ut fermentum in cursus. Ante consectetur id congue Ante consectetur id congue </p>
+                    {/* 12 */}
+                    <p className='col-span-4 row-span-2'>At a enim parturient id. Suspendisse ullamcorper fermentum accumsan diam tellus. Nibh pretium ultrices scelerisque dolor at etiam lectus gravida sed. Sit in turpis suspendisse et aliquam. Vulputate sit phasellus proin eget arcu. Enim nec ante velit erat nibh nunc amet. Tellus at sit imperdiet non. Cras dictum curabitur urna mauris in.
+                    Ut dui odio sagittis ut imperdiet ultricies mauris ac. A sit id etiam vitae non posuere tristique. Morbi sit mi sed nam amet tristique tellus. Sed quam aliquam pharetra nunc ipsum mattis. Volutpat pellentesque cras euismod habitant sit nibh. Dictum odio at aliquam sed. Orci odio lacinia id sem sed suspendisse mi fringilla purus. Quis sed ultricies ac nullam odio. Gravida sollicitudin viverra ornare pretium etiam tortor imperdiet tellus. Id purus etiam nunc hendrerit quam in. Dui netus lectus nulla cursus ultrices nulla. Morbi sit in gravida fermentum. Non sed lobortis amet eget sed donec.At a enim parturient id. Suspendisse ullamcorper fermentum accumsan diam tellus. Nibh pretium ultrices scelerisque dolor at etiam lectus gravida sed. Sit in turpis suspendisse et aliquam. Vulputate sit phasellus proin eget arcu. Enim nec ante velit erat nibh nunc amet. Tellus at sit imperdiet non. Cras dictum curabitur urna mauris in.
+                    Ut dui odio sagittis ut imperdiet ultricies mauris ac. A sit id etiam vitae non posuere tristique. Morbi sit mi sed nam amet tristique tellus. Sed quam aliquam pharetra nunc ipsum mattis. Volutpat pellentesque cras euismod habitant sit nibh. Dictum odio at aliquam sed. Orci odio lacinia id sem.</p>
+                    {/* 13 */}
+                    <p className='col-span-3'>At a enim parturient id. Suspendisse ullamcorper fermentum accumsan diam tellus. Nibh pretium ultrices scelerisque dolor at etiam lectus gravida sed. Sit in turpis suspendisse et aliquam.
+                    At a enim parturient id. Suspendisse ullamcorper fermentum accumsan diam tellus. Nibh pretium ultrices scelerisque dolor at etiam lectus gravida sed. Sit in turpis suspendisse et aliquam. Vulputate sit phasellus proin eget arcu. Enim nec ante velit erat nibh nunc amet. Tellus at sit imperdiet non. Cras dictum curabitur urna mauris in.
+                    Ut dui odio sagittis ut imperdiet ultricies mauris ac. A sit id etiam vitae non posuere tristique. Morbi sit mi sed nam amet tristique tellus. Sed quam aliquam pharetra.</p>
                     </div>
                 </div>
             </div>
